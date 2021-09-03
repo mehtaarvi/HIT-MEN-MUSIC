6b5c42b74ceb7ea6b3be60a5e0467d680dfa83f3
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2021-09-03 17:16:30 +0530

    Add files via upload

---
+++ modules/Private.py
@@ -0,0 +1,72 @@
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+import signal
+import os
+import sys
+
+
+HOME_TEXT = "<b>ʜᴇʏ, [{}](tg://user?id={})\n\n• ɪ'ᴍ 𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝• ᴀᴍᴀɴ━࿇ɢᴜᴊᴊᴀʀ࿇ᴍᴜs̶ɪᴄ♡︎ʙᴏᴛ
+ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ʏᴏᴜʀ ᴏᴘ ɢʀᴏᴜᴘ ᴅᴇᴠᴇʟᴏᴩᴇᴅ ʙʏ DARKAMAN\n• ɪ ᴄᴀɴ ᴍᴀɴᴀɢᴇ ᴠᴄ's\n\n• ʜɪᴛ /ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs.</b>"
+HELP = """
+🎧 <b>I Can Play Musics On VoiceChats 🤪</b>
+
+🎶 **Common Commands**:
+• `/song` __Download Song from youtube__
+• `/play`  __Play song you requested__
+• `/help` __Show help for commands__
+• `/dplay` __Play song you requested via deezer__
+• `splay` __Play song you requested via jio saavn__
+• `/ytplay` __Play song directly from youtube server__
+• `/search` __Search video songs links__
+• `/current` __Show now playing__
+• `/playlist` __Show now playing list__
+• `/video` __Downloads video song quickly__
+🎶 **Admin Commands**:
+• `/player`  __Open music player settings panel__
+• `/pause` __Pause song play__
+• `/skip` __Skip next song__
+• `/resume`  __Resume song play__
+• `/userbotjoin`  __Invites assistant to your chat__
+• `/end` __Stops music play__
+• `/admincache` __Refresh list of admins with vc power__
+© Powered By 
+[ __@DARKAMANSUPPORT || @DARKAMANSUPPORT__ ]
+"""
+
+
+
+@Client.on_message(filters.command('start'))
+async def start(client, message):
+    buttons = [
+       [
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ❀', url='https://t.me/DARKXV2BOT'),
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ɢʀᴏᴜᴘ❀', url='https://t.me/DARKAMANSUPPORT')
+                ],[
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴄʜᴀɴɴᴇʟ❀', url='https://t.me/DARKAMANCHANNEL'),
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴅᴀʀᴋᴀᴍᴀɴ❀', url='https://t.me/DARKAMAN')
+                ],[
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻ᴄᴏᴍᴍᴀɴᴅs', url='https://telegra.ph/𖣔ᴀᴍᴀɴ-ɢᴜᴊᴊᴀʀ-ᴍᴜsɪᴄ-08-31'),
+    ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo="https://telegra.ph/file/221de501e2f547a6359dd.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    await message.delete()
+
+
+@Client.on_message(filters.command("help"))
+async def show_help(client, message):
+    buttons = [
+        [
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢᴛᴏᴜᴘ❀', url='DARKXV2BOT'),
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ɢʀᴏᴜᴘ❀', url='https://t.me/DARKAMANSUPPORT')
+                ],[
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴄʜᴀɴɴᴇʟ❀', url='https://DARKAMANCHANNEL'),
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻❀ᴅᴀʀᴋᴀᴍᴀɴ❀', url='https://t.me/DARKAMAN')
+                ],[
+                InlineKeyboardButton('𖣔ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ༆⃝•👻ᴄᴏᴍᴍᴀɴᴅs', url='https://telegra.ph/𖣔ᴀᴍᴀɴ-ɢᴜᴊᴊᴀʀ-ᴍᴜsɪᴄ-08-31'),
+       ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo="https://telegra.ph/file/221de501e2f547a6359dd.jpg", caption=HELP, reply_markup=reply_markup)
+    await message.delete()
